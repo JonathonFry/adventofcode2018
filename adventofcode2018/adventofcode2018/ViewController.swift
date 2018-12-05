cb733518ed02deda2d718f3e42b0c78ca84a005b
Jonathon Fry <jonfry22@gmail.com>
2018-12-05 15:56:56 +0000

    shiny

--- adventofcode2018/adventofcode2018/ViewController.swift
+++ adventofcode2018/adventofcode2018/ViewController.swift
@@ -11,33 +11,46 @@ import Cocoa
 class ViewController: NSViewController {
     
     @IBOutlet weak var label: NSTextField!
+    @IBOutlet weak var time: NSTextField!
+    
+    @IBOutlet weak var loading: NSProgressIndicator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var output : [String] = []
+        DispatchQueue.global(qos: .background).async {
+            var output : [String] = []
+            let startTime = CFAbsoluteTimeGetCurrent()
+            
+            let day1 = Day1()
+            output.append(day1.part1())
+            output.append(day1.part2())
+            
+            let day2 = Day2()
+            output.append(day2.part1())
+            output.append(day2.part2())
+            
+            let day3 = Day3()
+            output.append(day3.part1())
+            output.append(day3.part2())
+            
+            let day4 = Day4()
+            output.append(day4.part1())
+            output.append(day4.part2())
+            
+            let day5 = Day5()
+            output.append(day5.part1())
+            output.append(day5.part2())
+            
+            let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+            
+            DispatchQueue.main.async {
+                self.loading.isHidden = true
+                self.label.stringValue = "\(output.joined(separator: "\n"))"
+                self.time.stringValue = "\(Double(timeElapsed))s"
+            }
+        }
         
-        let day1 = Day1()
-        output.append(day1.part1())
-        output.append(day1.part2())
-
-        let day2 = Day2()
-        output.append(day2.part1())
-        output.append(day2.part2())
-
-        let day3 = Day3()
-        output.append(day3.part1())
-        output.append(day3.part2())
-
-        let day4 = Day4()
-        output.append(day4.part1())
-        output.append(day4.part2())
-        
-        let day5 = Day5()
-        output.append(day5.part1())
-        output.append(day5.part2())
-        
-        label.stringValue = "\(output.joined(separator: "\n"))"
     }
 }
 
