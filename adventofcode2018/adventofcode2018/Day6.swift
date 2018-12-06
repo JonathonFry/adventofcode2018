76c317b6fe8d69414027db158ba55926980e5334
Jonathon Fry <jonfry22@gmail.com>
2018-12-06 11:12:20 +0000

    Day 6 part 2

--- adventofcode2018/adventofcode2018/Day6.swift
+++ adventofcode2018/adventofcode2018/Day6.swift
@@ -68,7 +68,50 @@ class Day6 {
     }
     
     func part2() -> String {
-        return "Day 6 part 2: "
+        let data = FileUtils.loadFile(name: "day6")
+        let array = FileUtils.split(data: data)
+        
+        let coords: [Pair<Int, Int>] =  array.map {
+            
+            let s = $0.split(separator: ",").map{ String($0).replacingOccurrences(of: " ", with: "") }
+            
+            return Pair(values:(Int(s[0])!, Int(s[1])!))
+        }
+        
+        let maxX = coords.map{ $0.values.0 }.max()!
+        let maxY = coords.map{ $0.values.1 }.max()!
+        let minX = coords.map{ $0.values.0 }.min()!
+        let minY = coords.map{ $0.values.1 }.min()!
+        
+        var board = [Pair<Int, Int>]()
+        
+        for i in minY...maxY {
+            for j in minX...maxX {
+                board.append(Pair(values: (j, i)))
+            }
+        }
+        
+        
+        var safeRegion = [Pair<Int, Int>]()
+        
+        for location in board {
+            var distances = Array<Int>()
+            
+            for coord in coords {
+                let distance = taxicabDistance(start: coord, end: location)
+                
+                distances.append(distance)
+            }
+            
+            if distances.reduce(0, +) < 10000 {
+                safeRegion.append(location)
+            }
+        
+        }
+        
+        let solution = safeRegion.count
+        
+        return "Day 6 part 2: \(solution)"
     }
     
     func taxicabDistance(start: Pair<Int, Int>, end: Pair<Int, Int>) -> Int {
